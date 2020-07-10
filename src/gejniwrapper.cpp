95c2f238c39824c6213d55834b305cf5427d2ffd
Keshizin <fab.takeshi@gmail.com>
2020-07-10 13:50:30 -0300

    Inclusão do código fonte em Java (BPMNParser)

--- src/gejniwrapper.cpp
+++ src/gejniwrapper.cpp
@@ -16,6 +16,10 @@ int GEJNIWrapper::startJVM()
 	JavaVMInitArgs vm_args;
 	JavaVMOption jvmopt[1];
 
+	// char path[200]="-Djava.class.path=src;lib\\camunda-bpmn-model-7.13.0-alpha3.jar;lib\\camunda-xml-model-7.13.0-alpha3.jar;";
+	std::string classpath = "-Djava.class.path=src\\java;";
+	jvmopt[0].optionString = (char*)classpath.c_str();
+
 	vm_args.version = JNI_VERSION_1_2;
 	vm_args.nOptions = 1;
 	vm_args.options = jvmopt;
@@ -27,6 +31,30 @@ int GEJNIWrapper::startJVM()
 	{
 		std::cout << "(!) ERROR It was not possible to create JVM." << std::endl;
 		return 0;
+	}
+
+	jclass jclass = env->FindClass("BPMNParser");
+
+	if(jclass == NULL)
+	{
+		std::cout << "(!) Java class not found." << std::endl;
+		env->ExceptionDescribe();
+		jvm->DestroyJavaVM();
+		return 0;
+	}
+
+	jmethodID methodId = env->GetStaticMethodID(jclass, "print", "()V");
+
+	if(methodId == NULL)
+		std::cout << "(!) ERROR Method is not found!" << std::endl;
+	else
+	{
+		env->CallStaticObjectMethod(jclass, methodId);
+
+		if (env->ExceptionCheck()) {
+			env->ExceptionDescribe();
+			env->ExceptionClear();
+		}
 	}
 
 	jvm->DestroyJavaVM();
---
+++ src/java/BPMNParser.java
@@ -0,0 +1,13 @@
+/*
+	BPMN Parser (java)
+
+	This file is part of the BPM Game Engine.
+
+	Copyright (C) 2020 Fabio Takeshi Ishikawa
+*/
+
+public class BPMNParser {
+	public static void print() {
+		System.out.println("BPMN Parser for BPM Game Engine!");
+	}
+}
